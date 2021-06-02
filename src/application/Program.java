0ae90963cef7877adb7d1540d873d50892b78888
Mateus Macial <mateus_macial@hotmail.com>
2021-06-01 21:26:17 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("ola mundo");
+		
+		Position pos = new Position(2, 2);
+		
+		System.out.println(pos);
 
 	}
 
